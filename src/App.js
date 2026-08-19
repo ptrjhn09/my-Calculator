47c455368992c49de3c3b686fd6517cca0949311
ptrjhn09 <peter.ricarte@ext.essilor.com>
2026-08-19 13:36:27 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [addNum, setNum] = useState (1);
+  const [multiplyNum, setMultiplyNum] = useState (1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      
+    <div>
+
+      <button onClick={()=> setNum(addNum +1)}>add</button><br/><br/>
+      <p>{addNum}</p>
+
+      <button onClick={()=> setMultiplyNum(multiplyNum * 2)}>Multiply</button>
+      <p>{multiplyNum}</p>
+
+      <button onClick={()=> {setNum(0); setMultiplyNum(1);}}>Clear</button>
+
     </div>
+      
   );
 }
 
