46043051141217ce11b14d64a11c56e686036ef3
ptrjhn09 <peter.ricarte@ext.essilor.com>
2026-08-20 09:37:30 +0800

    feat-addDivision

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
 
   const [addNum, setNum] = useState (1);
   const [multiplyNum, setMultiplyNum] = useState (1);
+  const [divisionNum, setDivisionNum] = useState (1);
 
   return (
 
@@ -18,7 +19,10 @@ function App() {
       <button onClick={()=> setMultiplyNum(multiplyNum * 2)}>Multiply</button>
       <p>{multiplyNum}</p>
 
-      <button onClick={()=> {setNum(0); setMultiplyNum(1);}}>Clear</button>
+       <button onClick={()=> setDivisionNum(divisionNum / 2)}>Multiply</button>
+       <p>{divisionNum}</p>
+
+        <button onClick={()=> {setNum(0); setMultiplyNum(1); setDivisionNum(1)}}>Clear</button>
 
     </div>
       
